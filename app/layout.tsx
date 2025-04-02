fee7429507601c12995cac50248ed4a039c02d7d
Gian Cabigting <gdcabigting.dev@gmail.com>
2025-04-02 17:53:31 +0800

    build: set up initial project structure for improved project structure
    This commit establishes the foundational directory structure for the project. The new project structure aims to improve code maintainability, separation of concerns, and overall project clarity.

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/assets/style/globals.css"
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Simple Projex",
+  description:
+    "A partner-specific web application focused on streamlining the creation of proposals and contracts through a template-based system.",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
