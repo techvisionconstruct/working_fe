ef10a7c46dc1e70f11cc0152eb12643739ab33e3
Gian Cabigting <gian@simpleprojex.com>
2025-04-04 02:05:14 +0800

    style: removed scrollArea

--- app/(site)/proposals/page.tsx
+++ app/(site)/proposals/page.tsx
@@ -10,6 +10,7 @@ import {
   TabsContent,
   ScrollArea,
   Button,
+  Separator,
 } from "@/components/shared";
 import { SortByComponent } from "@/components/shared/ui/sort-by-component";
 import { SearchComponent } from "@/components/shared/ui/search-component";
@@ -45,75 +46,51 @@ export default function ProposalPage() {
   };
 
   return (
-    <motion.div
-      className="container mx-auto py-8"
-      initial="hidden"
-      animate="visible"
-      variants={pageVariants}
-    >
-      <motion.div
-        className="flex justify-between items-center mb-6"
-        initial={{ y: 20, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.4 }}
-      >
-        <div className="flex flex-col">
-          <h1 className="text-3xl font-bold">Proposals</h1>
-          <p>Manage and track client proposals.</p>
-        </div>
-        <Button className="uppercase font-bold">New Proposal</Button>
-      </motion.div>
+    <div>
       <Tabs defaultValue="grid">
-        <motion.div
-          className="flex justify-between items-center mb-4"
-          initial={{ y: 15, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.3, delay: 0.1 }}
-        >
-          <div className="flex items-center gap-3 flex-wrap">
-            <SearchComponent
-              onChange={handleSearchChange}
-              value={searchQuery}
-            />
-            <SortByComponent
-              onChange={handleSortChange}
-              initialValue={sortOption.value}
-            />
+        <div className="sticky top-0 z-10 w-full left-0 bg-background">
+          <div className="container mx-auto pt-6 pb-2">
+            <div className="flex justify-between items-center mb-6">
+              <div className="flex flex-col">
+                <h1 className="text-3xl font-bold">Proposals</h1>
+                <p>Manage and track client proposals.</p>
+              </div>
+              <Button className="uppercase font-bold">New Proposals</Button>
+            </div>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center gap-3 flex-wrap">
+                <SearchComponent
+                  onChange={handleSearchChange}
+                  value={searchQuery}
+                />
+                <SortByComponent
+                  onChange={handleSortChange}
+                  initialValue={sortOption.value}
+                />
+              </div>
+              <TabsList>
+                <TabsTrigger value="grid">Grid View</TabsTrigger>
+                <TabsTrigger value="list">List View</TabsTrigger>
+              </TabsList>
+            </div>
+            <Separator className="mt-4" />
           </div>
-          <TabsList>
-            <TabsTrigger value="grid">Grid View</TabsTrigger>
-            <TabsTrigger value="list">List View</TabsTrigger>
-          </TabsList>
-        </motion.div>
-        <TabsContent value="grid" className="">
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.3, delay: 0.2 }}
-          >
-            <ScrollArea className="h-[calc(100vh-210px)]">
-              <ProposalGridView
-                sortOption={sortOption}
-                searchQuery={searchQuery}
-              />
-            </ScrollArea>
-          </motion.div>
-        </TabsContent>
-        <TabsContent value="list">
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.3, delay: 0.2 }}
-          >
-            <ScrollArea className="h-[calc(100vh-210px)]">
-              <ProposalListView
-                sortOption={sortOption}
-                searchQuery={searchQuery}
-              />
-            </ScrollArea>
-          </motion.div>
-        </TabsContent>
+        </div>
+        <div className="container mx-auto">
+          <TabsContent value="grid">
+            <ProposalGridView
+              sortOption={sortOption}
+              searchQuery={searchQuery}
+            />
+          </TabsContent>
+          <TabsContent value="list">
+            <ProposalListView
+              sortOption={sortOption}
+              searchQuery={searchQuery}
+            />
+          </TabsContent>
+        </div>
       </Tabs>
-    </motion.div>
+    </div>
   );
 }
--- app/(site)/templates/page.tsx
+++ app/(site)/templates/page.tsx
@@ -7,6 +7,7 @@ import {
   TabsTrigger,
   TabsContent,
   Button,
+  Separator,
 } from "@/components/shared";
 import { SortByComponent } from "@/components/shared/ui/sort-by-component";
 import { SearchComponent } from "@/components/shared/ui/search-component";
@@ -29,35 +30,45 @@ export default function TemplatePage() {
   };
 
   return (
-    <div className="container mx-auto py-8">
-      <div className="flex justify-between items-center mb-6">
-        <div className="flex flex-col">
-          <h1 className="text-3xl font-bold">Templates</h1>
-          <p>Manage and track client templates.</p>
-        </div>
-        <Button className="uppercase font-bold">New Template</Button>
-      </div>
+    <div>
       <Tabs defaultValue="grid">
-        <div className="flex justify-between items-center mb-4">
-          <div className="flex items-center gap-3 flex-wrap">
-            <SearchComponent
-              onChange={handleSearchChange}
-              value={searchQuery}
-            />
-            <SortByComponent
-              onChange={handleSortChange}
-              initialValue={sortOption.value}
-            />
+        <div className="sticky top-0 z-10 w-full left-0 bg-background">
+          <div className="container mx-auto pt-6 pb-2">
+            <div className="flex justify-between items-center mb-6">
+              <div className="flex flex-col">
+                <h1 className="text-3xl font-bold">Templates</h1>
+                <p>Manage and track your templates.</p>
+              </div>
+              <Button className="uppercase font-bold">New Template</Button>
+            </div>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center gap-3 flex-wrap">
+                <SearchComponent
+                  onChange={handleSearchChange}
+                  value={searchQuery}
+                />
+                <SortByComponent
+                  onChange={handleSortChange}
+                  initialValue={sortOption.value}
+                />
+              </div>
+              <TabsList>
+                <TabsTrigger value="grid">Grid View</TabsTrigger>
+                <TabsTrigger value="list">List View</TabsTrigger>
+              </TabsList>
+            </div>
+            <Separator className="mt-4" />
           </div>
-          <TabsList>
-            <TabsTrigger value="grid">Grid View</TabsTrigger>
-            <TabsTrigger value="list">List View</TabsTrigger>
-          </TabsList>
         </div>
-        <TabsContent value="grid">
-          <TemplateGridView sortOption={sortOption} searchQuery={searchQuery} />
-        </TabsContent>
-        <TabsContent value="list"></TabsContent>
+        <div className="container mx-auto">
+          <TabsContent value="grid">
+            <TemplateGridView
+              sortOption={sortOption}
+              searchQuery={searchQuery}
+            />
+          </TabsContent>
+          <TabsContent value="list"></TabsContent>
+        </div>
       </Tabs>
     </div>
   );
