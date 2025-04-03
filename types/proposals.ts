e4a078083ef05a0156910a021b6dfa87a599f0f2
Gian Cabigting <gian@simpleprojex.com>
2025-04-04 07:50:28 +0800

    feat(create-proposal): added the components for create-proposal

--- types/proposals.ts
+++ types/proposals.ts
@@ -10,3 +10,48 @@ export interface ProposalsPageProps {
     id: number;
   };
 }
+export interface ProposalData {
+  id: number
+  title: string
+  description: string
+  categories: Category[]
+  variables: Variable[]
+  created_at: string
+  imageUrl: string
+  clientName?: string
+  clientEmail?: string
+  clientPhone?: string
+  useGlobalMarkup?: boolean
+  globalMarkupPercentage?: number
+}
+export interface Variable {
+  id: number
+  name: string
+  type: string
+  value?: string
+}
+export interface Element {
+  id: number
+  name: string
+  material_cost: string
+  labor_cost: string
+  markup_percentage: number
+  calculatedMaterialCost?: number
+  calculatedLaborCost?: number
+}
+export interface Category {
+  id: number
+  name: string
+  elements: ProposalElement[];
+};
+
+export type ProposalElement = {
+  id: number;
+  name: string;
+  material_cost: string;
+  labor_cost: string;
+  markup_percentage: number;
+};
+export interface Template extends Omit<ProposalData, "variables"> {
+  variables: Omit<Variable, "value">[]
+}
