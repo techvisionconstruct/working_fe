ebd30fad8832ae7859330c72ae1ea1fcf663984a
Gian Cabigting <gian@simpleprojex.com>
2025-04-23 05:52:31 +0800

    fix: updated the template types

--- types/templates.ts
+++ types/templates.ts
@@ -1,88 +1,55 @@
-import { SortOption } from "./sort";
-import { Element as ElementCategory } from "@/hooks/api/lookup/use-categories";
-
-export interface TemplateProps {
-  sortOption: SortOption;
-  searchQuery?: string;
+export interface Template {
+  id: number;
+  name: string;
+  description: string;
+  created_at: string;
+  updated_at: string;
+  image: string;
+  modules: Module[];
+  // template_elements: TemplateElement[];
+  parameters: Parameter[];
 }
 
-export interface TemplatePageProps {
-  params: {
-    id: number;
-  };
+export interface Module {
+  id: number;
+  name: string;
+  description: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface TemplateElement {
+  id: number;
+  element: Element;
+  material_cost: string;
+  labor_cost: string;
+  image: string;
+  module: Module2;
+  markup: number;
 }
 
 export interface Element {
   id: number;
   name: string;
   description: string;
+  created_at: string;
   formula: string;
   labor_formula: string;
-  image: string;
-  created_at: string;
   updated_at: string;
-}
-
-export interface Module {
-  id: number;
-  name: string;
-  elements: ElementCategory[];
-  // description: string;
-  // created_at: string;
-  // updated_at: string;
-}
-export interface Parameters {
-  id: number;
-  name?: string;
-  type?: string;
-  value?: string;
-}
-
-export interface TemplateElements {
-  id: number;
-  element: Element;
-  material_cost: string
-  labor_cost: string;
   image: string;
-  created_at: string;
-  updated_at: string;
-  module: Module;
 }
 
-export interface Template {
+export interface Module2 {
   id: number;
   name: string;
   description: string;
-  modules: Module[];
-  parameters: Parameters[];
-  template_elements: TemplateElements[];
   created_at: string;
   updated_at: string;
-  image: string;
 }
 
-export interface TemplateDetailsProps {
-  template: Template;
-  onUpdateTemplate: (template: Template) => void;
-  onNext: () => void;
-}
-
-export interface TemplateParametersProps {
-  parameter: Parameters[];
-  onUpdateTemplate: (updater: (prevTemplate: Template) => Template) => void;
-  onNext: () => void;
-  onPrevious: () => void;
-}
-
-export interface TemplateCategoriesProps {
-  template: Template;
-  onUpdateTemplate: (template: Template | ((prevTemplate: Template) => Template)) => void;
-  onNext: () => void;
-  onPrevious: () => void;
-}
-
-export interface TemplatePreviewProps {
-  template: Template;
-  onPrevious: () => void;
-  onSave: () => void;
+export interface Parameter {
+  id: number;
+  name: string;
+  value: number;
+  type: string;
 }
