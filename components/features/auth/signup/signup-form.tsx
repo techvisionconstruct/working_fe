69f571305e140f0157ffff940f11abbbdf555f21
Arwin <arwin@simpleprojex.com>
2025-04-25 02:16:22 +0800

    #86cyhympe feat(authentication): google buttons now added to new authentication pages

--- components/features/auth/signup/signup-form.tsx
+++ components/features/auth/signup/signup-form.tsx
@@ -42,6 +42,7 @@ import {
 } from "@/zod-schemas/register/register-schema";
 import { useRegister } from "@/hooks/api/auth/register";
 import { useVerifyOtp } from "@/hooks/api/auth/verify-otp";
+import { GoogleButton } from "../google-login/google-button";
 
 type PersonalInfoValues = z.infer<typeof personalInfoSchema>;
 type ContactInfoValues = z.infer<typeof contactInfoSchema>;
@@ -484,17 +485,7 @@ export function RegisterForm({
                 Or continue with
               </span>
             </div>
-            <div className="gap-4">
-              <Button variant="outline" className="w-full">
-                <svg viewBox="0 0 24 24" className="h-5 w-5 mr-2">
-                  <path
-                    d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                    fill="currentColor"
-                  />
-                </svg>
-                <span>Sign Up with Google</span>
-              </Button>
-            </div>
+            <GoogleButton className="w-full" />
             <Separator className="my-4" />
             <div className="text-center text-sm text-muted-foreground">
               Already have an account?{" "}
