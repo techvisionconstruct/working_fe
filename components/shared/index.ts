6c75b3076ab6c9f069761d724d9d2f9d7812a886
Gian Cabigting <gian@simpleprojex.com>
2025-05-06 00:12:31 +0800

    feat: added alert-dialog

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,235 +1,251 @@
 /**
  * Button
  */
-export { Button } from "./button/button"
-export { RainbowButton } from "./button/rainbow-button"
+export { Button } from "./button/button";
+export { RainbowButton } from "./button/rainbow-button";
 /**
  * Input
  */
-export { Input } from "./input/input"
+export { Input } from "./input/input";
 /**
  * Label
  */
-export { Label } from "./label/label"
+export { Label } from "./label/label";
 /**
  * SVG Icon
  */
-export { GoogleSVGIcon } from "./svg-icon/google-svg-icon"
-export { MSSVGIcon } from "./svg-icon/ms-svg-icon"
+export { GoogleSVGIcon } from "./svg-icon/google-svg-icon";
+export { MSSVGIcon } from "./svg-icon/ms-svg-icon";
 /**
  * SVG
  */
-export { DiagonalSeparatorSVG } from "./svg/diagonal-separator-svg"
+export { DiagonalSeparatorSVG } from "./svg/diagonal-separator-svg";
 /**
  * Card
  */
-export { Card } from "./card/card"
-export { CardHeader } from "./card/card-header"
-export { CardTitle } from "./card/card-title"
-export { CardDescription } from "./card/card-description"
-export { CardAction } from "./card/card-action"
-export { CardContent } from "./card/card-content"
-export { CardFooter } from "./card/card-footer"
+export { Card } from "./card/card";
+export { CardHeader } from "./card/card-header";
+export { CardTitle } from "./card/card-title";
+export { CardDescription } from "./card/card-description";
+export { CardAction } from "./card/card-action";
+export { CardContent } from "./card/card-content";
+export { CardFooter } from "./card/card-footer";
 /**
  * Tab
  */
-export { Tabs } from "./tabs/tabs"
-export { TabsList } from "./tabs/tabs-list"
-export { TabsTrigger } from "./tabs/tabs-trigger"
-export { TabsContent } from "./tabs/tabs-content"
+export { Tabs } from "./tabs/tabs";
+export { TabsList } from "./tabs/tabs-list";
+export { TabsTrigger } from "./tabs/tabs-trigger";
+export { TabsContent } from "./tabs/tabs-content";
 /**
  * Avatar
  */
-export { Avatar } from "./avatar/avatar"
-export { AvatarImage } from "./avatar/avatar-image"
-export { AvatarFallback } from "./avatar/avatar-fallback"
+export { Avatar } from "./avatar/avatar";
+export { AvatarImage } from "./avatar/avatar-image";
+export { AvatarFallback } from "./avatar/avatar-fallback";
 /**
  * Dialog
  */
-export { Dialog } from "./dialog/dialog"
-export { DialogTrigger } from "./dialog/dialog-trigger"
-export { DialogPortal } from "./dialog/dialog-portal"
-export { DialogClose } from "./dialog/dialog-close"
-export { DialogOverlay } from "./dialog/dialog-overlay"
-export { DialogContent } from "./dialog/dialog-content"
-export { DialogHeader } from "./dialog/dialog-header"
-export { DialogFooter } from "./dialog/dialog-footer"
-export { DialogTitle } from "./dialog/dialog-title"
-export { DialogDescription } from "./dialog/dialog-description"
+export { Dialog } from "./dialog/dialog";
+export { DialogTrigger } from "./dialog/dialog-trigger";
+export { DialogPortal } from "./dialog/dialog-portal";
+export { DialogClose } from "./dialog/dialog-close";
+export { DialogOverlay } from "./dialog/dialog-overlay";
+export { DialogContent } from "./dialog/dialog-content";
+export { DialogHeader } from "./dialog/dialog-header";
+export { DialogFooter } from "./dialog/dialog-footer";
+export { DialogTitle } from "./dialog/dialog-title";
+export { DialogDescription } from "./dialog/dialog-description";
 /**
  * Command
  */
-export { Command } from "./command/command"
-export { CommandDialog } from "./command/command-dialog"
-export { CommandInput } from "./command/command-input"
-export { CommandList } from "./command/command-list"
-export { CommandEmpty } from "./command/command-empty"
-export { CommandGroup } from "./command/command-group"
-export { CommandSeparator } from "./command/command-separator"
-export { CommandItem } from "./command/command-item"
-export { CommandShortcut } from "./command/command-shortcut"
+export { Command } from "./command/command";
+export { CommandDialog } from "./command/command-dialog";
+export { CommandInput } from "./command/command-input";
+export { CommandList } from "./command/command-list";
+export { CommandEmpty } from "./command/command-empty";
+export { CommandGroup } from "./command/command-group";
+export { CommandSeparator } from "./command/command-separator";
+export { CommandItem } from "./command/command-item";
+export { CommandShortcut } from "./command/command-shortcut";
 /**
  * Popover
  */
-export { Popover } from "./popover/popover"
-export { PopoverTrigger } from "./popover/popover-trigger"
-export { PopoverContent } from "./popover/popover-content"
-export { PopoverAnchor } from "./popover/popover-anchor"
+export { Popover } from "./popover/popover";
+export { PopoverTrigger } from "./popover/popover-trigger";
+export { PopoverContent } from "./popover/popover-content";
+export { PopoverAnchor } from "./popover/popover-anchor";
 /**
  * Select
  */
-export { Select } from "./select/select"
-export { SelectGroup } from "./select/select-group"
-export { SelectValue } from "./select/select-value"
-export { SelectTrigger } from "./select/select-trigger"
-export { SelectContent } from "./select/select-content"
-export { SelectScrollUpButton } from "./select/select-scroll-up-button"
-export { SelectScrollDownButton } from "./select/select-scroll-down-button"
-export { SelectLabel } from "./select/select-label"
-export { SelectItem } from "./select/select-item"
-export { SelectSeparator } from "./select/select-separator"
+export { Select } from "./select/select";
+export { SelectGroup } from "./select/select-group";
+export { SelectValue } from "./select/select-value";
+export { SelectTrigger } from "./select/select-trigger";
+export { SelectContent } from "./select/select-content";
+export { SelectScrollUpButton } from "./select/select-scroll-up-button";
+export { SelectScrollDownButton } from "./select/select-scroll-down-button";
+export { SelectLabel } from "./select/select-label";
+export { SelectItem } from "./select/select-item";
+export { SelectSeparator } from "./select/select-separator";
 /**
  * Dropdown Menu
  */
-export { DropdownMenu } from "./dropdown-menu/dropdown-menu"
-export { DropdownMenuPortal } from "./dropdown-menu/dropdown-menu-portal"
-export { DropdownMenuTrigger } from "./dropdown-menu/dropdown-menu-trigger"
-export { DropdownMenuContent } from "./dropdown-menu/dropdown-menu-content"
-export { DropdownMenuGroup } from "./dropdown-menu/dropdown-menu-group"
-export { DropdownMenuItem } from "./dropdown-menu/dropdown-menu-item"
-export { DropdownMenuCheckboxItem } from "./dropdown-menu/dropdown-menu-checkbox-item"
-export { DropdownMenuRadioGroup } from "./dropdown-menu/dropdown-menu-radio-group"
-export { DropdownMenuRadioItem } from "./dropdown-menu/dropdown-menu-radio-item"
-export { DropdownMenuLabel } from "./dropdown-menu/dropdown-menu-label"
-export { DropdownMenuSeparator } from "./dropdown-menu/dropdown-menu-separator"
-export { DropdownMenuShortcut } from "./dropdown-menu/dropdown-menu-shortcut"
-export { DropdownMenuSub } from "./dropdown-menu/dropdown-menu-sub"
-export { DropdownMenuSubTrigger } from "./dropdown-menu/dropdown-menu-sub-trigger"
-export { DropdownMenuSubContent } from "./dropdown-menu/dropdown-menu-sub-content"
+export { DropdownMenu } from "./dropdown-menu/dropdown-menu";
+export { DropdownMenuPortal } from "./dropdown-menu/dropdown-menu-portal";
+export { DropdownMenuTrigger } from "./dropdown-menu/dropdown-menu-trigger";
+export { DropdownMenuContent } from "./dropdown-menu/dropdown-menu-content";
+export { DropdownMenuGroup } from "./dropdown-menu/dropdown-menu-group";
+export { DropdownMenuItem } from "./dropdown-menu/dropdown-menu-item";
+export { DropdownMenuCheckboxItem } from "./dropdown-menu/dropdown-menu-checkbox-item";
+export { DropdownMenuRadioGroup } from "./dropdown-menu/dropdown-menu-radio-group";
+export { DropdownMenuRadioItem } from "./dropdown-menu/dropdown-menu-radio-item";
+export { DropdownMenuLabel } from "./dropdown-menu/dropdown-menu-label";
+export { DropdownMenuSeparator } from "./dropdown-menu/dropdown-menu-separator";
+export { DropdownMenuShortcut } from "./dropdown-menu/dropdown-menu-shortcut";
+export { DropdownMenuSub } from "./dropdown-menu/dropdown-menu-sub";
+export { DropdownMenuSubTrigger } from "./dropdown-menu/dropdown-menu-sub-trigger";
+export { DropdownMenuSubContent } from "./dropdown-menu/dropdown-menu-sub-content";
 /**
  * Badge
  */
-export { Badge } from "./badge/badge"
+export { Badge } from "./badge/badge";
 /**
  * Skeleton
  */
-export { Skeleton } from "./skeleton/skeleton"
+export { Skeleton } from "./skeleton/skeleton";
 /**
  * Pagination
  */
-export { Pagination } from "./pagination/pagination"
-export { PaginationContent } from "./pagination/pagination-content"
-export { PaginationItem } from "./pagination/pagination-item"
-export { PaginationLink } from "./pagination/pagination-link"
-export { PaginationPrevious } from "./pagination/pagination-previous"
-export { PaginationNext } from "./pagination/pagination-next"
-export { PaginationEllipsis } from "./pagination/pagination-ellipsis"
+export { Pagination } from "./pagination/pagination";
+export { PaginationContent } from "./pagination/pagination-content";
+export { PaginationItem } from "./pagination/pagination-item";
+export { PaginationLink } from "./pagination/pagination-link";
+export { PaginationPrevious } from "./pagination/pagination-previous";
+export { PaginationNext } from "./pagination/pagination-next";
+export { PaginationEllipsis } from "./pagination/pagination-ellipsis";
 /**
  * Table
  */
-export { Table } from "./table/table"
-export { TableHeader } from "./table/table-header"
-export { TableBody } from "./table/table-body"
-export { TableFooter } from "./table/table-footer"
-export { TableRow } from "./table/table-row"
-export { TableHead } from "./table/table-head"
-export { TableCell } from "./table/table-cell"
-export { TableCaption } from "./table/table-caption"
+export { Table } from "./table/table";
+export { TableHeader } from "./table/table-header";
+export { TableBody } from "./table/table-body";
+export { TableFooter } from "./table/table-footer";
+export { TableRow } from "./table/table-row";
+export { TableHead } from "./table/table-head";
+export { TableCell } from "./table/table-cell";
+export { TableCaption } from "./table/table-caption";
 /**
  * Checkbox
  */
-export { Checkbox } from "./checkbox/checkbox"
+export { Checkbox } from "./checkbox/checkbox";
 /**
  * Separator
  */
-export { Separator } from "./separator/separator"
+export { Separator } from "./separator/separator";
 /**
  * Sheet
  */
-export { Sheet } from "./sheet/sheet"
-export { SheetTrigger } from "./sheet/sheet-trigger"
-export { SheetClose } from "./sheet/sheet-close"
-export { SheetPortal } from "./sheet/sheet-portal"
-export { SheetOverlay } from "./sheet/sheet-overlay"
-export { SheetContent } from "./sheet/sheet-content"
-export { SheetHeader } from "./sheet/sheet-header"
-export { SheetFooter } from "./sheet/sheet-footer"
-export { SheetTitle } from "./sheet/sheet-title"
-export { SheetDescription } from "./sheet/sheet-description"
+export { Sheet } from "./sheet/sheet";
+export { SheetTrigger } from "./sheet/sheet-trigger";
+export { SheetClose } from "./sheet/sheet-close";
+export { SheetPortal } from "./sheet/sheet-portal";
+export { SheetOverlay } from "./sheet/sheet-overlay";
+export { SheetContent } from "./sheet/sheet-content";
+export { SheetHeader } from "./sheet/sheet-header";
+export { SheetFooter } from "./sheet/sheet-footer";
+export { SheetTitle } from "./sheet/sheet-title";
+export { SheetDescription } from "./sheet/sheet-description";
 /**
  * Tooltip
  */
-export { Tooltip } from "./tooltip/tooltip"
-export { TooltipTrigger } from "./tooltip/tooltip-trigger"
-export { TooltipContent } from "./tooltip/tooltip-content"
-export { TooltipProvider } from "@radix-ui/react-tooltip"
+export { Tooltip } from "./tooltip/tooltip";
+export { TooltipTrigger } from "./tooltip/tooltip-trigger";
+export { TooltipContent } from "./tooltip/tooltip-content";
+export { TooltipProvider } from "@radix-ui/react-tooltip";
 /**
  * Switch
  */
-export { Switch } from "./switch/switch"
+export { Switch } from "./switch/switch";
 /**
  * Textarea
  */
-export { Textarea } from "./textarea/textarea"
+export { Textarea } from "./textarea/textarea";
 /**
  * Hover Card
  */
-export { HoverCard } from "./hover-card/hover-card"
-export { HoverCardTrigger } from "./hover-card/hover-cart-trigger"
-export { HoverCardContent } from "./hover-card/hover-card-content"
+export { HoverCard } from "./hover-card/hover-card";
+export { HoverCardTrigger } from "./hover-card/hover-cart-trigger";
+export { HoverCardContent } from "./hover-card/hover-card-content";
 /**
  * Accordion
  */
-export { Accordion } from "./accordion/accordion"
-export { AccordionItem } from "./accordion/accordion-item"
-export { AccordionTrigger } from "./accordion/accordion-trigger"
-export { AccordionContent } from "./accordion/accordion-content"
+export { Accordion } from "./accordion/accordion";
+export { AccordionItem } from "./accordion/accordion-item";
+export { AccordionTrigger } from "./accordion/accordion-trigger";
+export { AccordionContent } from "./accordion/accordion-content";
 /**
  * Scroll Area
  */
-export { ScrollArea } from "./scroll-area/scroll-area"
-export { ScrollBar } from "./scroll-area/scroll-bar"
+export { ScrollArea } from "./scroll-area/scroll-area";
+export { ScrollBar } from "./scroll-area/scroll-bar";
 /**
  * Alert
  */
-export { Alert } from "./alert/alert"
-export { AlertTitle } from "./alert/alert-title"
-export { AlertDescription } from "./alert/alert-description"
+export { Alert } from "./alert/alert";
+export { AlertTitle } from "./alert/alert-title";
+export { AlertDescription } from "./alert/alert-description";
 /**
  * Slider
  */
-export { Slider } from "./slider/slider"
+export { Slider } from "./slider/slider";
 /**
  * Form
  */
-export { Form } from "./form/form"
-export { FormField, FormFieldContext } from "./form/form-field"
-export { FormItem, FormItemContext } from "./form/form-item"
-export { FormLabel } from "./form/form-label"
-export { FormControl } from "./form/form-control"
-export { FormMessage } from "./form/form-message"
-export { FormDescription } from "./form/form-description"
+export { Form } from "./form/form";
+export { FormField, FormFieldContext } from "./form/form-field";
+export { FormItem, FormItemContext } from "./form/form-item";
+export { FormLabel } from "./form/form-label";
+export { FormControl } from "./form/form-control";
+export { FormMessage } from "./form/form-message";
+export { FormDescription } from "./form/form-description";
 /**
  * Radio Group
  */
-export { RadioGroup } from "./radio-group/radio-group"
-export { RadioGroupItem } from "./radio-group/radio-group-item" 
+export { RadioGroup } from "./radio-group/radio-group";
+export { RadioGroupItem } from "./radio-group/radio-group-item";
 /**
  * Toaster
  */
-export { Toaster } from "./toaster/toaster"
+export { Toaster } from "./toaster/toaster";
 /**
  * Progress
  */
-export { Progress } from "./progress/progress"
+export { Progress } from "./progress/progress";
 /**
  * Input OTP
  */
-export { InputOTP } from "./input-otp/input-otp"
-export { InputOTPSeparator } from "./input-otp/input-otp-separator"
-export { InputOTPSlot } from "./input-otp/input-otp-slot"
-export { InputOTPGroup } from "./input-otp/input-otp-group"
+export { InputOTP } from "./input-otp/input-otp";
+export { InputOTPSeparator } from "./input-otp/input-otp-separator";
+export { InputOTPSlot } from "./input-otp/input-otp-slot";
+export { InputOTPGroup } from "./input-otp/input-otp-group";
 /**
  * VisuallyHidden
  */
-export { VisuallyHidden } from "./visually-hidden/visually-hidden"
+export { VisuallyHidden } from "./visually-hidden/visually-hidden";
+/**
+ * Alert Dialog
+ */
+export {
+  AlertDialog,
+  AlertDialogPortal,
+  AlertDialogOverlay,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogAction,
+  AlertDialogCancel,
+} from "./alert-dialog/alert-dialog";
