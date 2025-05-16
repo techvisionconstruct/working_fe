c276d0b9849983c74424caef759a987b7cde88ec
Gian Cabigting <gian@simpleprojex.com>
2025-05-17 06:22:17 +0800

    feat: missing dto

--- types/user/dto.ts
+++ types/user/dto.ts
@@ -1,12 +1,17 @@
 export interface UserResponse {
-    id: string
-    email: string
-    username: string
-    first_name: string
-    last_name: string
-    role: string
-    is_superuser: boolean
-    is_staff: boolean
-    is_active: boolean
-    subscription?: any
-  }
+  id: string;
+  email: string;
+  username: string;
+  first_name: string;
+  last_name: string;
+  role: string;
+  is_superuser: boolean;
+  is_staff: boolean;
+  is_active: boolean;
+  subscription?: any;
+}
+
+export interface ErrorItem {
+  field: string;
+  message: string;
+}
